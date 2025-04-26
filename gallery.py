5e86d3eae792ff9e8eeea65f9e6ecc31f5f97810
jpoppe99 <150079598+jpoppe99@users.noreply.github.com>
2025-04-26 02:18:58 -0700

    moving stuff around
    flask?

---
+++ gallery.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def gallery():
+	images = os.listdir('assets/screenshots')
+	out=[]
+	for img in images:
+		if img.lower().endswith(('png','jpg','jpeg','gif')):
+			imgurl = url_for('static',filename='static/screenshots/{img}')
+			out.append(imgurl)
+	return render_template('gallary.html',out)
+
+if __name__ == '__main__':
+	app.run(debug=True)
